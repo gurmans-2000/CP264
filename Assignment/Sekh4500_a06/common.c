d497373001cca7e439e47ebda26ec94f2d3b08fd
gurmans-2000 <78860622+gurmans-2000@users.noreply.github.com>
2021-03-14 22:37:03 -0400

    Create common.c

---
+++ Assignment/Sekh4500_a06/common.c
@@ -0,0 +1,46 @@
+/*--------------------------------------------------
+ Project: CP264-a6
+ File:    common.c
+ Description: implementation of common.h
+ Author:  HBF
+ Version: 2021-02-24
+ --------------------------------------------------
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "common.h"
+
+NODE* new_node(int data, int type) {
+	NODE *np = (NODE*) malloc(sizeof(NODE));
+	// Successful memory allocation
+	if (np != NULL) {
+		np->data = data;
+		np->type = type;
+		np->next = NULL;
+	}
+	return np;
+}
+
+void display(NODE *start) {
+	while (start) {
+		if (start->type == 0)
+			printf("%d ", start->data);
+		else
+			printf("%c ", start->data);
+
+		start = start->next;
+	}
+}
+
+void clean(NODE **topp) {
+	NODE *curr = *topp;
+	NODE *prev = NULL;
+
+	while (curr != NULL) {
+		prev = curr;
+		curr = curr->next;
+		free(prev);
+	}
+	*topp = NULL;
+}
